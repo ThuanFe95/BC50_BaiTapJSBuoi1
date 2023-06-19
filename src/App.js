7a4c7366625f6fe38b4f5617fb6bf93f2593c6f7
ThuanFe95 <thuannghiem175@gmail.com>
2023-06-19 19:17:48 +0700

    Bai Tap JS Buoi 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BaiTapThucHanhLayOut from './BaiTapThucHanhLayOut';
+
+function App() {
+  return (
+    <div>
+      <BaiTapThucHanhLayOut />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BaiTapThucHanhLayOut/index.js
@@ -0,0 +1,17 @@
+import Header from "./header";
+import Banner from "./banner";
+import Item from "./item";
+import Footer from "./footer";
+
+function BaiTapThucHanhLayOut(){
+    return(
+        <div>
+            <Header />
+            <Banner />
+            <Item />
+            <Footer />
+        </div>
+    );
+}
+
+export default BaiTapThucHanhLayOut;
